b0d29c7bf8d00f79fe0379d415a28ca8d07abae6
Greg <greghory.a@outlook.com>
2024-11-29 07:49:04 -0300

    Bugs fixed

--- Levels/trunk_puzzle_introduction.gd
+++ Levels/trunk_puzzle_introduction.gd
@@ -5,7 +5,7 @@ extends Node2D
 	"Ele parece estar trancado, deve guardar algo muito valioso...",
 	"Mas veja só, tem 4 chaves e uma mensagem perto dele",
 	"A mensagem diz que apenas a chave certa abrirá o baú",
-	"caso a chave errada seja usada 3 vezes, o baú irá se destruir!",
+	"Caso a chave errada seja usada 3 vezes, a expressão irá mudar.",
 	"para abrirmos o baú, devemos usar a chave que contem uma expressão de mesmo valor que no cadeado do baú!",
 ]
 
@@ -34,15 +34,18 @@ func _ready() -> void:
 	Dialogic.start('Quiz')
 	get_viewport().set_input_as_handled()
 
+	
+
 func _input(event: InputEvent):
 	if first_time:
-		if (event is InputEventKey and (event.keycode == KEY_ENTER or event.keycode == KEY_KP_ENTER) and event.pressed) or (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
+		if (event is InputEventKey and (event.keycode == KEY_TAB or event.keycode == KEY_KP_ENTER) and event.pressed) or (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
 			if falas_speak <= falas.size() - 1:
 				DisplayServer.tts_stop()
 				DisplayServer.tts_speak(falas[falas_speak], speaker)
 				falas_speak+=1
 			elif (first_time):
 				first_time = false
+		
 	else:
 		Dialogic.end_timeline()
 		get_tree().change_scene_to_file("res://Levels/Trunk_Puzzle.tscn")
--- scripts/World.gd
+++ scripts/World.gd
@@ -17,8 +17,14 @@ extends Node2D
 
 var falas_speak = 0
 var errors = 0;
+var first_time = true;
 var write_anw
-var speak_enum 
+var speak_enum
+var s_op_1
+var s_op_2
+var s_op_3
+var s_op_4 
+var s_enum
 var congrats = "Parabéns viajante! Você recebeu uma moeda dourada!"
 var explain = "Você errou, viajante! Para abrirmos o baú, o resultado da expressão da chave deve ser o mesmo resultado da expressão do baú! Você consegue, vamos tentar outra vez!"
 var explain_basic = "Não parece certo, vamos tentar outra vez!"
@@ -39,28 +45,39 @@ var options = [
 func _ready():
 	_quiz()
 	AutoloadScene.previous_scene = "res://Levels/Trunk_Puzzle.tscn"
+
+func _convert_hifen_to_minus(new_text):
+	var c_new_text = new_text.replace("-"," menos")
+	return c_new_text
 	
 func _random_keys():
 	var random_index = randi() % options.size()
 	var option = options[random_index]
+	errors = 0;
 	op_1.text = option[0]
+	s_op_1 = _convert_hifen_to_minus(option[0])
 	op_2.text = option[1]
+	s_op_2 = _convert_hifen_to_minus(option[1])
 	op_3.text = option[2]
+	s_op_3 = _convert_hifen_to_minus(option[2])
 	op_4.text = option[3]
+	s_op_4 = _convert_hifen_to_minus(option[3])
 	enumc.text = option[4]
+	s_enum = _convert_hifen_to_minus(option[4])
 	write_anw = option[5]
-	speak_enum = "Chave 1:"+op_1.text+"Chave 2:"+op_2.text+"Chave  3:"+op_3.text+"Chave 4:"+op_4.text+"Báu:"+enumc.text+"Aperte R para repetir!"
+	speak_enum = "Chave 1| "+s_op_1+"Chave 2| "+s_op_2+"Chave  3| "+s_op_3+"Chave 4| "+s_op_4+"Báu "+s_enum+"Aperte R para repetir!"
 
 func _on_line_edit_text_submitted(new_text):
 	if new_text == write_anw :
 		write_ans()
-	else:
+	elif (errors < 2):
 		line_ed.text = ""
 		Dialogic.start('wrong_simple')
 		DisplayServer.tts_speak(explain_basic, speaker)
 		errors+= 1
-		if errors == 3 :
-			wrong_ans()
+		
+	elif (errors == 2) :
+		wrong_ans()
 
 func _quiz():
 	_random_keys()
@@ -69,16 +86,22 @@ func _quiz():
 
 func _input(event: InputEvent):
 	
-	if (event is InputEventKey and (event.keycode == KEY_ENTER or event.keycode == KEY_KP_ENTER) and event.pressed) or (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
+	if (event is InputEventKey and first_time and (event.keycode == KEY_TAB or event.keycode == KEY_ENTER) and event.pressed) or (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
 		if (line_ed.text!=""):
 			DisplayServer.tts_stop()
 			_on_line_edit_text_submitted(line_ed.text)
 
-	if event is InputEventKey and event.keycode != KEY_ENTER and event.keycode != KEY_KP_ENTER and event.pressed:
+	if event is InputEventKey and (event.keycode != KEY_TAB or event.keycode != KEY_ENTER) and event.pressed and first_time:
 		if event.keycode >= KEY_KP_0 and event.keycode <= KEY_KP_9 :
+			Dialogic.end_timeline()
 			line_ed.text= str(OS.get_keycode_string(event.physical_keycode)).substr(3,1)
+			DisplayServer.tts_stop()
+			DisplayServer.tts_speak(line_ed.text, speaker)
 		elif event.keycode >= KEY_0 and event.keycode <= KEY_9:
+			Dialogic.end_timeline()
 			line_ed.text = str(OS.get_keycode_string(event.physical_keycode))
+			DisplayServer.tts_stop()
+			DisplayServer.tts_speak(line_ed.text, speaker)
 		elif event.keycode == KEY_R :
 			speak_exerc()
 
@@ -98,6 +121,7 @@ func _input(event: InputEvent):
 			get_tree().change_scene_to_file("res://scenes/menu/menu_pausa.tscn")
 			
 func write_ans():
+	first_time = false;
 	keys.visible = false
 	Trunk.play("Open")
 	op_s.play()
@@ -105,16 +129,20 @@ func write_ans():
 	coin.visible = true
 	Dialogic.start('rg_ans')
 	DisplayServer.tts_speak(congrats, speaker)
-	reset_t.start()
+	
 
 func wrong_ans():
 	Dialogic.start('wrong_ans')
 	DisplayServer.tts_speak(explain, speaker)
-	reset_t.wait_time = 10
+	reset_t.wait_time = 15
 	reset_t.start()
 
 func _on_reset_timer_timeout():
-	get_tree().reload_current_scene()
+	Dialogic.end_timeline()
+	line_ed.text = ""
+	_random_keys()
+	speak_exerc()
+	#get_tree().reload_current_scene()
 	
 func speak_exerc():
 	DisplayServer.tts_stop()
